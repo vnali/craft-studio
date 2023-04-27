ed65b9d50b86500f1a909c0243ca1d03d0689c50
vnali <vnali.dev@gmail.com>
2023-04-27 08:43:52 +0000

    Clarify 'Save other user episodes' and 'Create episodes'

--- src/Studio.php
+++ src/Studio.php
@@ -728,7 +728,7 @@ class Studio extends Plugin
                         'label' => Craft::t('studio', 'Create episodes', [
                             'name' => $podcast->title,
                         ]),
-                        'info' => Craft::t('studio', 'Includes saving an episode as published. For publishing other user draft episodes, user also need save other user drafts and save other user episodes permission'),
+                        'info' => Craft::t('studio', "Includes saving an episode as published. For publishing other user draft episodes, user also need 'save other user drafts'. If the creator of canonical version of episode is another user, the user needs 'save other user episodes' permission too"),
                     ];
                     $nestedViewEpisodes['studio-createDraftEpisodes-' . $podcast->uid] = [
                         'label' => Craft::t('studio', 'Create draft episodes', [
